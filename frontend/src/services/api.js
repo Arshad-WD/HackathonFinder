8e9a350dfae218bf033cc91ab44866907d16fe16
ARSHAD CHAUDHARY <arshadchaudhary9819@gmail.com>
2025-12-10 11:41:03 +0530

    api changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,11 +1,16 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL ;
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export async function fetchEvents() {
-  const res = await fetch(`${BASE_URL}/events`);
+  const res = await fetch(`${BASE_URL}/api/events`);
+  if (!res.ok) throw new Error("Failed to fetch events");
   return res.json();
 }
 
 export async function triggerFetch() {
-  const res = await fetch(`${BASE_URL}/fetch`);
+  const res = await fetch(`${BASE_URL}/trigger`, {
+    method: "POST"
+  });
+
+  if (!res.ok) throw new Error("Failed to trigger fetch");
   return res.json();
 }
