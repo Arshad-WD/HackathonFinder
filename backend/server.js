7cae3129beedb5577887b47eae637d29e27d0078
ARSHAD CHAUDHARY <arshadchaudhary9819@gmail.com>
2025-12-10 11:21:31 +0530

    Enabled Cors

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,10 @@ import "./cron.js"
 import { runDeepSearch } from "./runSearch.js";
 
 const app = express();
-app.use(cors(
-   origin= "*",
-    methods= ["GET", "POST"]
-));
+app.use(cors({
+   origin: "*",
+    methods: ["GET", "POST"]
+}));
 
 const RESULT_FILE = "results.json";
 
